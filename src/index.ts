014e73a2f826e3e7dcb6ae691a45479cf7d77c28
uKaxg <uka.pgr@gmail.com>
2024-08-31 16:37:50 +0700

    chore: Add resolvers for game, review, and author by ID

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,16 @@ const resolvers = {
     author: (_: any, args: { id: string }) =>
       db.authors.find((author) => author.id === args.id),
   },
+  Game: {
+    reviews: (parent: { id: string }) =>
+      db.reviews.filter((review) => review.game_id === parent.id),
+  },
+  Review: {
+    game: (parent: { game_id: string }) =>
+      db.games.find((game) => game.id === parent.game_id),
+    author: (parent: { author_id: string }) =>
+      db.authors.find((author) => author.id === parent.author_id),
+  },
 };
 const server = new ApolloServer({
   typeDefs,
