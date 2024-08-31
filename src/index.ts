9647b91314859ba38fd12747382befae7dd72228
uKaxg <uka.pgr@gmail.com>
2024-08-31 16:00:31 +0700

    chore: Add GraphQL query resolvers for game, review, and author by ID

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ import db from "./_db.js";
 const resolvers = {
   Query: {
     games: () => db.games,
+    game: (_: any, args: { id: string }) =>
+      db.games.find((game) => game.id === args.id),
     reviews: () => db.reviews,
+    review: (_: any, args: { id: string }) =>
+      db.reviews.find((review) => review.id === args.id),
     authors: () => db.authors,
+    author: (_: any, args: { id: string }) =>
+      db.authors.find((author) => author.id === args.id),
   },
 };
 const server = new ApolloServer({
